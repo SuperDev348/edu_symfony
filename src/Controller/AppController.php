3efb1b09ce5103401d86bdee148d6267f7ac765b
VladimirDanil <vladimir.danil25@mail.ru>
2021-06-08 17:37:15 -0700

    fix

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -1,5 +1,5 @@
 <?php
-namespace App\Controller;
+namespace App\Controller;    
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
