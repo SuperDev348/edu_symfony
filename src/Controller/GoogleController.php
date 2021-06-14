91f08d802c52e31c96f3c6511409962fdb64498f
VladimirDanil <vladimir.danil25@mail.ru>
2021-06-14 11:57:34 -0700

    some update

--- src/Controller/GoogleController.php
+++ src/Controller/GoogleController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;  
+namespace App\Controller;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
