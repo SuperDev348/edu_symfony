baae999f86e564cd254d1261403846fef483c0d6
VladimirDanil <vladimir.danil25@mail.ru>
2021-08-29 18:22:00 -0700

    update

--- src/Controller/NotificationController.php
+++ src/Controller/NotificationController.php
@@ -1,6 +1,6 @@
 <?php
 namespace App\Controller;
-
+   
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,4 +15,4 @@ class NotificationController extends AbstractController
         return $this->render('pages/notification/index.html.twig', [
         ]);
     }
-}
+} 
