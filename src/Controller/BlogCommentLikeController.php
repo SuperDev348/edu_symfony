dd015cffe2a7bf1ceb19559517547971a952ba97
SuperDev348 <vladimir.danil25@mail.ru>
2021-12-23 02:31:51 -0800

    update

--- src/Controller/BlogCommentLikeController.php
+++ src/Controller/BlogCommentLikeController.php
@@ -3,9 +3,9 @@ namespace App\Controller;
       
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-// use Symfony\Component\Routing\Annotation\Route;
-// use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-// use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\PropertyAccess\PropertyAccess;
