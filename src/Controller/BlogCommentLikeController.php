d951eed6de9c059bb3b3b2dd07293a7235dc02cc
SuperDev348 <vladimir.danil25@mail.ru>
2022-02-07 07:33:50 -0800

    some parts fixing

--- src/Controller/BlogCommentLikeController.php
+++ src/Controller/BlogCommentLikeController.php
@@ -9,9 +9,9 @@ use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\PropertyAccess\PropertyAccess;
-// use App\Entity\User;
-// use App\Entity\BlogCommentLike;
-// use App\Entity\BlogComment;
+use App\Entity\User;
+use App\Entity\BlogCommentLike;
+use App\Entity\BlogComment;
 
 class BlogCommentLikeController extends AbstractController
 {
