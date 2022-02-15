20995c07d38fa40fc2df6569c48acc3eee138cdb
SuperDev348 <vladimir.danil25@mail.ru>
2022-02-15 05:45:29 -0800

    fixed

--- src/Controller/ContactusController.php
+++ src/Controller/ContactusController.php
@@ -9,7 +9,6 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\PropertyAccess\PropertyAccess;
 use App\Entity\Listing;
-use App\Entity\UserRequest;
 
 class ContactusController extends AbstractController
 {
