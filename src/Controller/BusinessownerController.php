0cd0f59c73449e271c0a2ae0bc07722489813dd0
SuperDev348 <vladimir.danil25@mail.ru>
2022-05-16 10:07:42 -0800

    fix

--- src/Controller/BusinessownerController.php
+++ src/Controller/BusinessownerController.php
@@ -10,9 +10,6 @@ use PhpParser\Builder;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
-use Symfony\Component\Routing\Annotation\Route;
-
 /**
  * @Route("/businessowner")
  */
