77c0dc5ffe9297eda8fa65628f8ea61dd82d945a
SuperDev348 <vladimir.danil25@mail.ru>
2023-03-16 07:48:00 +0100

    update

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -12,6 +12,9 @@ class ProfileController extends AbstractController
      */
     public function index(): Response
     {
-        
+        return $this->render('pages/profile/index.html.twig', [
+            'page' => 'profile',
+            'subtitle' => 'Profile Setting',
+        ]);
     }
 }
