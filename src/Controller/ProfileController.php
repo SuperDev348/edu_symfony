38f8de1001b8c1051ffe0340fd84eb3b19245d31
SuperDev348 <vladimir.danil25@mail.ru>
2023-04-23 12:50:57 +0200

    update profile

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -7,14 +7,4 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProfileController extends AbstractController
 {
-    /**
-     * @Route("/profile", name="profile")
-     */
-    public function index(): Response
-    {
-        return $this->render('pages/profile/index.html.twig', [
-            'page' => 'profile',
-            'subtitle' => 'Profile Setting',
-        ]);
-    }
 }
