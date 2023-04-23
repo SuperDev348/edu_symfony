70947430b7910365362c4040acf310ee946059c5
SuperDev348 <vladimir.danil25@mail.ru>
2023-04-23 12:51:08 +0200

    update pricing

--- src/Controller/PricingController.php
+++ src/Controller/PricingController.php
@@ -10,9 +10,4 @@ class PricingController extends AbstractController
     /**
      * @Route("/pricing", name="pricing")
      */
-    public function index(): Response
-    {
-        return $this->render('pages/pricing/index.html.twig', [
-        ]);
-    }
 }
