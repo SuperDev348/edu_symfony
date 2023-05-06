be09f7b3dbcff575baeb38f135b693231e72ede1
SuperDev348 <vladimir.danil25@mail.ru>
2023-05-06 12:27:36 +0200

    fix

--- src/Controller/CityController.php
+++ src/Controller/CityController.php
@@ -20,6 +20,18 @@ class CityController extends AbstractController
         $this->session = $session;
     }
 
+    private function isAuth() {
+        if(is_null($this->session->get('user'))){
+            return false;
+        }
+        $user = $this->getDoctrine()->getRepository(User::class)->find($this->session->get('user')->getId());
+        if ($user->getBan()) {
+            $this->session->clear();
+            return false;
+        }
+        return true;
+    }
+
     private function isAdmin() {
         if(is_null($this->session->get('user'))||$this->session->get('user')->getType()!="admin"){
             return false;
