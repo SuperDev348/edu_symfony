94a0a639dfc72547f12061e1579749db08d92d69
SuperDev348 <vladimir.danil25@mail.ru>
2023-05-09 12:26:04 +0200

    update home controller

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -4,3 +4,17 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ProfileController extends AbstractController
+{
+    /**
+     * @Route("/profile", name="profile")
+     */
+    public function index(): Response
+    {
+        return $this->render('pages/profile/index.html.twig', [
+            'page' => 'profile',
+            'subtitle' => 'Profile Setting',
+        ]);
+    }
+}
