d0e4d910980580108ea8c3bf255e9ae2b9d0fbe0
BenjaminS348 <coco.dragon002@outlook.com>
2024-03-19 07:15:04 +0100

    fix

--- src/Controller/WishlistController.php
+++ src/Controller/WishlistController.php
@@ -21,6 +21,19 @@ class WishlistController extends AbstractController
         $this->session = $session;
     }
 
+
+    private function isAdmin() {
+        if(is_null($this->session->get('user'))||$this->session->get('user')->getType()!="admin"){
+            return false;
+        }
+        $user = $this->getDoctrine()->getRepository(User::class)->find($this->session->get('user')->getId());
+        if ($user->getBan()) {
+            $this->session->clear();
+            return false;
+        }
+        return true;
+    }
+
     /**
      * @Route("/wishlist", name="wishlist")
      */
